1e9a31bc9250bd7c1f1c6daa02795e88e70479db
wujincheng <wujincheng@neusoft.com>
2021-10-21 17:16:51 +0800

    finished the first part

--- app/src/main/java/com/wujincheng/drysister/MainActivity.java
+++ app/src/main/java/com/wujincheng/drysister/MainActivity.java
@@ -3,12 +3,61 @@ package com.wujincheng.drysister;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button showBtn;
+    private ImageView showImg;
+    private ArrayList<String>urls;
+    private int curPos = 0;
+    private PictureLoader loader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loader = new PictureLoader();
+        bindView();
+        initData();
+    }
+    private void bindView(){
+        showBtn = (Button) findViewById(R.id.btnView);
+        showImg = (ImageView) findViewById(R.id.loadImg);
+
+        showBtn.setOnClickListener(this);
+    }
+
+    private  void initData(){
+        urls = new ArrayList<String>();
+        urls.add("http://ww4.sinaimg.cn/large/610dc034jw1f6ipaai7wgj20dw0kugp4.jpg");
+        urls.add("http://ww3.sinaimg.cn/large/610dc034jw1f6gcxc1t7vj20hs0hsgo1.jpg");
+        urls.add("http://ww4.sinaimg.cn/large/610dc034jw1f6f5ktcyk0j20u011hacg.jpg");
+        urls.add("http://ww1.sinaimg.cn/large/610dc034jw1f6e1f1qmg3j20u00u0djp.jpg");
+        urls.add("http://ww3.sinaimg.cn/large/610dc034jw1f6aipo68yvj20qo0qoaee.jpg");
+        urls.add("http://ww3.sinaimg.cn/large/610dc034jw1f69c9e22xjj20u011hjuu.jpg");
+        urls.add("http://ww3.sinaimg.cn/large/610dc034jw1f689lmaf7qj20u00u00v7.jpg");
+        urls.add("http://ww3.sinaimg.cn/large/c85e4a5cjw1f671i8gt1rj20vy0vydsz.jpg");
+        urls.add("http://ww2.sinaimg.cn/large/610dc034jw1f65f0oqodoj20qo0hntc9.jpg");
+        urls.add("http://ww2.sinaimg.cn/large/c85e4a5cgw1f62hzfvzwwj20hs0qogpo.jpg");
+    }
+
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnView:
+                if(curPos > 9){
+                    curPos = 0;
+                }
+                loader.load(showImg,urls.get(curPos));
+                curPos++;
+                break;
+        }
     }
 }
